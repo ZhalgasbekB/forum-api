e7964405e7c34ba5c8e93c27c403dc5518acd1e1
QWERTYUI <zhbolatov>
2024-04-01 21:33:08 +0600

    AAA

--- internal/model/likeComment.go
+++ internal/model/likeComment.go
@@ -1,7 +1,9 @@
 package model
 
 type LikeComment struct {
-	UserId int `json:"user_id"`
-	PostId int `json:"post_id"`
-	Vote   int `json:"vote"`
+	UserId    int  `json:"user_id"`
+	CommentId int  `json:"post_id"`
+	IsLike    bool `json:"vote"`
+	Up        int  `json:"up"`
+	Down      int  `json:"down"`
 }
--- migrations/comments_like.sql
+++ migrations/comments_like.sql
@@ -1,5 +1,9 @@
-CREATE TABLE IF NOT EXISTS posts_votes (
-    post_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS comments_likes (
     user_id INTEGER NOT NULL,
-    vote INTEGER NOT NULL
+    comment_id INTEGER NOT NULL,
+    is_like BOOLEAN DEFAULT true NOT NULL,
+    up INTEGER NOT NULL,
+    down INTEGER NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES comments(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
