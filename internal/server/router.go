fbd2cf843538d394d724bdb0f2fc6699cedfcd1f
zhbolatov <gfk575@gmail.com>
2024-04-05 15:04:26 +0600

    D3

--- internal/server/router.go
+++ internal/server/router.go
@@ -11,25 +11,25 @@ type Router interface {
 func NewRouter(h *Handler) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/userd3", h.Home)                                           // (GET METHOD) get all posts
 	mux.HandleFunc("/login", h.Login)                                           // (POST METHOD)
 	mux.HandleFunc("/register", h.Register)                                     // (POST METHOD)
 	mux.Handle("/logout", h.RequiredAuthentication(http.HandlerFunc(h.Logout))) // (POST METHOD)
+	mux.HandleFunc("/d3", h.Home)                                               // (GET METHOD) get all posts
 
-	mux.Handle("/userd3/user-likes", h.RequiredAuthentication(http.HandlerFunc(h.LikedPostsUser))) // (GET METHOD)			  // ???
-	mux.Handle("/userd3/user-posts", h.RequiredAuthentication(http.HandlerFunc(h.PostsUser)))      // (GET METHOD) user posts // ???
+	mux.Handle("/d3/user-likes", h.RequiredAuthentication(http.HandlerFunc(h.LikedPostsUser))) // (GET METHOD)			  // ???
+	mux.Handle("/d3/user-posts", h.RequiredAuthentication(http.HandlerFunc(h.PostsUser)))      // (GET METHOD) user posts // ???
 
-	mux.Handle("/userd3/post", h.RequiredAuthentication(http.HandlerFunc(h.Post)))               // (GET METHOD) post and his comments
-	mux.Handle("/userd3/post-create", h.RequiredAuthentication(http.HandlerFunc(h.CreatePosts))) // (POST METHOD) create post
-	mux.Handle("/userd3/post-update", h.RequiredAuthentication(http.HandlerFunc(h.UpdatePost)))  // (PUT METHOD) update
-	mux.Handle("/userd3/post-delete", h.RequiredAuthentication(http.HandlerFunc(h.DeletePost)))  // (DELETE METHOD) delete
+	mux.Handle("/d3/post", h.RequiredAuthentication(http.HandlerFunc(h.Post)))                  // (GET METHOD) post and his comments
+	mux.Handle("/d3/post-create", h.RequiredAuthentication(http.HandlerFunc(h.CreatePosts)))    // (POST METHOD) create post
+	mux.Handle("/d3/post-update", h.RequiredAuthentication(http.HandlerFunc(h.UpdatePost)))     // (PUT METHOD) update
+	mux.Handle("/userd3/post-delete", h.RequiredAuthentication(http.HandlerFunc(h.DeletePost))) // (DELETE METHOD) delete
 
-	mux.Handle("/userd3/post-like", h.RequiredAuthentication(http.HandlerFunc(h.LikePosts)))       // (POST METHOD)
+	mux.Handle("/d3/post-like", h.RequiredAuthentication(http.HandlerFunc(h.LikePosts)))           // (POST METHOD)
 	mux.Handle("/userd3/comment-like", h.RequiredAuthentication(http.HandlerFunc(h.LikeComments))) // (POST METHOD)
 
-	mux.Handle("/userd3/comment-create", h.RequiredAuthentication(http.HandlerFunc(h.CreateComment))) // (POST METHOD) create
-	mux.Handle("/userd3/comment-update", h.RequiredAuthentication(http.HandlerFunc(h.UpdateComment))) // (PUT METHOD) update
-	mux.Handle("/userd3/comment-delete", h.RequiredAuthentication(http.HandlerFunc(h.DeleteComment))) // (DELETE METHOD) delete
+	mux.Handle("/d3/comment-create", h.RequiredAuthentication(http.HandlerFunc(h.CreateComment))) // (POST METHOD) create
+	mux.Handle("/d3/comment-update", h.RequiredAuthentication(http.HandlerFunc(h.UpdateComment))) // (PUT METHOD) update
+	mux.Handle("/3/comment-delete", h.RequiredAuthentication(http.HandlerFunc(h.DeleteComment)))  // (DELETE METHOD) delete
 
 	return h.IsAuthenticated(mux)
 }
