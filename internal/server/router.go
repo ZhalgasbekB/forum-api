aa6042250195991da37d24729bdf7a2151c33386
zhalgasbolatov <gfk575@gmail.com>
2024-04-07 15:47:39 +0500

    ADDA

--- internal/server/router.go
+++ internal/server/router.go
@@ -11,15 +11,14 @@ type Router interface {
 func NewRouter(h *Handler) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/login", h.Login)                                           // 200 (POST METHOD)
-	mux.HandleFunc("/register", h.Register)                                     // 201 (POST METHOD)
-	mux.Handle("/logout", h.RequiredAuthentication(http.HandlerFunc(h.Logout))) // 200 (POST METHOD)
-	mux.HandleFunc("/d3", h.Home)                                               // 200 (GET METHOD) get all posts
+	mux.HandleFunc("/d3", h.Home)           // 200 (GET METHOD) get all posts
+	mux.HandleFunc("/login", h.Login)       // 200 (POST METHOD)
+	mux.HandleFunc("/register", h.Register) // 201 (POST METHOD)
 
+	mux.Handle("/logout", h.RequiredAuthentication(http.HandlerFunc(h.Logout)))                // 200 (POST METHOD)
+	mux.Handle("/d3/category", h.RequiredAuthentication(http.HandlerFunc(h.PostCategory)))     // 200 (GET METHOD) user posts
 	mux.Handle("/d3/user-likes", h.RequiredAuthentication(http.HandlerFunc(h.LikedPostsUser))) // 200 (GET METHOD)
 	mux.Handle("/d3/user-posts", h.RequiredAuthentication(http.HandlerFunc(h.PostsUser)))      // 200 (GET METHOD) user posts
-
-	mux.Handle("/d3/category", h.RequiredAuthentication(http.HandlerFunc(h.PostCategory))) // 200 (GET METHOD) user posts
 
 	mux.Handle("/d3/post", h.RequiredAuthentication(http.HandlerFunc(h.Post)))                  // 200    	(GET METHOD) post and his comments id ?? category
 	mux.Handle("/d3/post-create", h.RequiredAuthentication(http.HandlerFunc(h.CreatePosts)))    // 201   // (POST METHOD) create post
