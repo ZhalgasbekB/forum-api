dd5e3bd6717a6dba2553b2471134f5abcbece17f
zhbolatov <gfk575@gmail.com>
2024-04-07 12:26:43 +0600

    ADD CATEGORY

--- internal/server/router.go
+++ internal/server/router.go
@@ -19,13 +19,15 @@ func NewRouter(h *Handler) http.Handler {
 	mux.Handle("/d3/user-likes", h.RequiredAuthentication(http.HandlerFunc(h.LikedPostsUser))) // 200 (GET METHOD)
 	mux.Handle("/d3/user-posts", h.RequiredAuthentication(http.HandlerFunc(h.PostsUser)))      // 200 (GET METHOD) user posts
 
+	// mux.Handle("/d3/category", h.RequiredAuthentication(http.HandlerFunc(h.PostCategory))) // 200 (GET METHOD) user posts
+
 	mux.Handle("/d3/post", h.RequiredAuthentication(http.HandlerFunc(h.Post)))                  // 200    	(GET METHOD) post and his comments
 	mux.Handle("/d3/post-create", h.RequiredAuthentication(http.HandlerFunc(h.CreatePosts)))    // 201   // (POST METHOD) create post
 	mux.Handle("/d3/post-update", h.RequiredAuthentication(http.HandlerFunc(h.UpdatePost)))     // 202   // (PUT METHOD) update
 	mux.Handle("/userd3/post-delete", h.RequiredAuthentication(http.HandlerFunc(h.DeletePost))) // 202   // (DELETE METHOD) delete
 
-	mux.Handle("/d3/post-like", h.RequiredAuthentication(http.HandlerFunc(h.LikePosts)))           // 200  // (POST METHOD)
-	mux.Handle("/userd3/comment-like", h.RequiredAuthentication(http.HandlerFunc(h.LikeComments))) // 200  // (POST METHOD)
+	mux.Handle("/d3/post-like", h.RequiredAuthentication(http.HandlerFunc(h.LikePosts)))       // 200  // (POST METHOD)
+	mux.Handle("/d3/comment-like", h.RequiredAuthentication(http.HandlerFunc(h.LikeComments))) // 200  // (POST METHOD)
 
 	mux.Handle("/d3/comment-create", h.RequiredAuthentication(http.HandlerFunc(h.CreateComment))) // 201 	(POST METHOD) create
 	mux.Handle("/d3/comment-update", h.RequiredAuthentication(http.HandlerFunc(h.UpdateComment))) // 202	(PUT METHOD) update
