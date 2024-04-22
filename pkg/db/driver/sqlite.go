aa27fac72f48c054a4607278093457ee90721c83
zhalgasbolatov <gfk575@gmail.com>
2024-04-22 08:01:32 +0500

    SSS

--- pkg/db/driver/sqlite.go
+++ pkg/db/driver/sqlite.go
@@ -6,7 +6,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func NewDB(cfg config.Config) (*sql.DB, error) {
+func NewDB(cfg config.Config) (*sql.DB, error) { // DB
 	db, err := sql.Open(cfg.Db.Driver, cfg.Db.Dsn)
 	if err != nil {
 		return nil, err
