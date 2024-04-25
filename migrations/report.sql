7130c15ba2f373a6e6b74e7e04c978532ed52247
zhbolatov <zhbolatov1>
2024-04-25 09:38:58 +0600

    QWE

--- migrations/report.sql
+++ migrations/report.sql
@@ -2,8 +2,8 @@ CREATE TABLE reports (
     report_id INTEGER PRIMARY KEY AUTOINCREMENT,
     post_id INTEGER,
     comment_id INTEGER,
-    reported_by INTEGER NOT NULL,
-    assigned_to INTEGER,
+    moderator INTEGER NOT NULL,
+    admin INTEGER,
     status TEXT NOT NULL DEFAULT 'open' CHECK (status IN ('open', 'closed', 'ignored')),
     reason TEXT NOT NULL,
     admin_response TEXT,
@@ -11,6 +11,6 @@ CREATE TABLE reports (
     updated_at TIMESTAMP DEFAULT (datetime('now')),
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE SET NULL,
     FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE SET NULL,
-    FOREIGN KEY (reported_by) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (assigned_to) REFERENCES users(id) ON DELETE SET NULL
+    FOREIGN KEY (moderator) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (admin) REFERENCES users(id) ON DELETE SET NULL
 );
