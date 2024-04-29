4f748a657000a84ca7c38b448884c75fa3747e96
zhbolatov <EEUU>
2024-04-29 12:29:04 +0600

    ADD 2

--- internal/server/nothification.go
+++ internal/server/nothification.go
@@ -2,6 +2,23 @@ package server
 
 import "net/http"
 
-func (h *Handler) Noth(w http.ResponseWriter, r *http.Request)  {}
-func (h *Handler) Noth1(w http.ResponseWriter, r *http.Request) {}
-func (h *Handler) Noth2(w http.ResponseWriter, r *http.Request) {}
+func (h *Handler) Noth(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func (h *Handler) Noth1(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func (h *Handler) Noth2(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
