dcbdda0ce83dd992566da7b43dd7ee33b302b3a4
zhbolatov <EEUU>
2024-04-29 12:47:15 +0600

    TABLE NOTHIFCATION

--- migrations/nothification.sql
+++ migrations/nothification.sql
@@ -10,5 +10,4 @@ CREATE TABLE IF NOT EXISTS nothifications(
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
     FOREIGN KEY (created_user_id) REFERENCES users(id) ON DELETE CASCADE
-
 ) 
