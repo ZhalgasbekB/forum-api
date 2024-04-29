7519badf832a1aa7a30de7244cf681fce3e40a1c
zhbolatov <EEUU>
2024-04-29 10:24:32 +0600

    ADVANCED

--- internal/model/nothification.go
+++ internal/model/nothification.go
@@ -1,1 +1,26 @@
 package model
+
+import "time"
+
+type Nothification struct {
+	ID                int       `json:"id"`
+	UserID            int       `json:"user_id"`
+	TypeNoth          string    `json:"type"`
+	PostID            int       `json:"post_id"`
+	CreatedNothUserID int       `json:"noth_user_id"`
+	Message           string    `json:"message"`
+	ISRead            bool      `json:"is_read"`
+	CreatedAt         time.Time `json:"create_at"`
+}
+
+type NothificationDTO struct {
+	ID int `json:"id"`
+}
+
+type NothificationDTO1 struct {
+	ID int `json:"id"`
+}
+
+type NothificationDTO2 struct {
+	ID int `json:"id"`
+}
--- internal/repository/nothification/nothification.go
+++ internal/repository/nothification/nothification.go
@@ -1,1 +1,40 @@
 package nothification
+
+import "database/sql"
+
+const query = ""
+
+type NothificationRepository struct {
+	DB *sql.DB
+}
+
+func InitNothificationRepository(db *sql.DB) *NothificationRepository {
+	return &NothificationRepository{
+		DB: db,
+	}
+}
+
+func (n *NothificationRepository) Create() error {
+	if _, err := n.DB.Exec(query); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (n *NothificationRepository) Read() error {
+	return nil
+}
+
+func (n *NothificationRepository) Update() error {
+	if _, err := n.DB.Exec(query); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (n *NothificationRepository) Delete() error {
+	if _, err := n.DB.Exec(query); err != nil {
+		return err
+	}
+	return nil
+}
