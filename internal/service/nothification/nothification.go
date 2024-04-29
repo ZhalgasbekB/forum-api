dfc5c89a5e2785211ebe25dd4ce04549d037f5f2
zhbolatov <EEUU>
2024-04-29 10:36:29 +0600

    CREATING CRUD NOTHINCATION

--- internal/service/nothification/nothification.go
+++ internal/service/nothification/nothification.go
@@ -1,1 +1,41 @@
 package nothification
+
+type INothificationRepository interface {
+	Create() error
+	Read() error
+	Delete() error
+	Update() error
+}
+
+type INothificationService interface {
+	CreateService() error
+	ReadService() error
+	DeleteService() error
+	UpdateService() error
+}
+
+type NothificationService struct {
+	nothificationRepository INothificationRepository
+}
+
+func InitNothificationService(nothificationRepository INothificationRepository) *NothificationService {
+	return &NothificationService{
+		nothificationRepository: nothificationRepository,
+	}
+}
+
+func (ns *NothificationService) CreateService() error {
+	return nil
+}
+
+func (ns *NothificationService) ReadService() error {
+	return nil
+}
+
+func (ns *NothificationService) DeleteService() error {
+	return nil
+}
+
+func (ns *NothificationService) UpdateService() error {
+	return nil
+}
